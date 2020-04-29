9c26d41570f981da0d481192107010757ee67f4b
grzesiakm <mrtgrzesiak@gmail.com>
2020-04-29 19:06:35 +0200

    django startproject and sta startapp

---
+++ django_igarden/igarden/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'igarden/home.html')
+
+def lists(request):
+    return render(request, 'igarden/lists.html')
+
