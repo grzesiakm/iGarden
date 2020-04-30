7603e1ee12d706bae44eaf4c99ca2117c3c1d86c
grzesiakm <mrtgrzesiak@gmail.com>
2020-04-30 19:07:08 +0200

    navbar

--- igarden/urls.py
+++ igarden/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='igarden-home'),
-    path('lists/', views.lists, name='igarden-my-lists'),
+    path('lists/', views.lists, name='igarden-lists'),
 ]
