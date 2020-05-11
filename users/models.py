4242665ead7fbbf916a90d22d04adde6f029cc17
grzesiakm <mrtgrzesiak@gmail.com>
2020-05-11 15:26:19 +0200

    Flower model added - accesible from admin page

--- users/models.py
+++ users/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-# Create your models here.
 
+# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -19,7 +19,3 @@ class Profile(models.Model):
             img.thumbnail(output_size)
             img.save(self.image.path)
 
-
-class Flower(models.Model):
-    description = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='flowers')
