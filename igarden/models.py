ba67105cbcd5c931c374ad96d019ff26e4854475
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-05-15 19:24:07 +0200

    connected ml..?

--- igarden/models.py
+++ igarden/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
 class Flower(models.Model):
     name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='images/', blank=True)
--- igarden/views.py
+++ igarden/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from .models import Flower
+from model.model import Model
+from PIL import Image
 
 
 def home(request):
@@ -13,9 +15,11 @@ def lists(request):
 
 
 def search(request):
+    model = Model()
+
     if request.method == 'POST':
-        # f_file = request.FILES['flower_file']
-        # print(f_file.name)
-        # (nazwa, index) = find_kwiatek_mondre_maszyny(f_file)
-        return render(request, 'igarden/detail.html', {'flower': Flower.objects.filter(name='Daisy')[0]})
+        f_file = request.FILES['flower_file']
+        img = Image.open(f_file)
+        found_name = model.predict(img)
+        return render(request, 'igarden/detail.html', {'flower': Flower.objects.filter(name=found_name)[0]})
     return render(request, 'igarden/search.html')
