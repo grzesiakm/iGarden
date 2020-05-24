51a272b60c2b70b940278e645fd7a567a96923a8
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-05-24 21:07:45 +0200

    urls auth moved

--- django_igarden/urls.py
+++ django_igarden/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('igarden.urls')),
+    path('admin/', admin.site.urls),
     path('lists/', include('lists.urls')),
     path('users/', include('users.urls')),
 ]
