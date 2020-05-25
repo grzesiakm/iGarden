a0ae08bcd7cb76bddbd4c7e2e7b9b2a3172e85a9
grzesiakm <mrtgrzesiak@gmail.com>
2020-05-25 22:08:55 +0200

    celery dont like me

--- django_igarden/celery.py
+++ django_igarden/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_igarden.settings')
-app = Celery('django_igarden')
+app = Celery('reminders')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
