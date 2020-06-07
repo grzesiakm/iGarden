ef70dccede1e7a02dcdd68c38aff70f36f582494
MagdalenaGie <magdalenaxgorska@gmail.com>
2020-06-07 20:09:56 +0200

    favourites works

--- igarden/views.py
+++ igarden/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.contrib.auth.decorators import login_required
 from PIL import Image
 from model.model import Model
 from .models import Flower
@@ -57,6 +57,7 @@ def detail(request):
         return render(request, 'igarden/explore.html', {'form': form})
 
 
+@login_required
 def add_to_list(request, id):
     if request.method == 'POST':
         form = AddToListForm(request.POST)
@@ -71,4 +72,19 @@ def add_to_list(request, id):
     else:
         form = AddToListForm()
         return render(request, 'igarden/list_add.html', {'form': form})
+
+
+@login_required
+def add_to_fav(request, id):
+    id_ = id;
+    obj = get_object_or_404(Flower, id=id_)
+    if not UserFlowersList.objects.filter(name="Favourites"):
+        new_list = UserFlowersList()
+        new_list.name = "Favourites"
+        new_list.owner = request.user
+        new_list.save()
+    chosen_list = get_object_or_404(UserFlowersList, name="Favourites")
+    chosen_list.elements.add(obj)
+    messages.success(request, f"Flower has been successfully added to favourites!")
+    return render(request, 'lists/list_detail.html', {'object': chosen_list})
 
