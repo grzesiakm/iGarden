ecbbe30cd897e3751c3e1fc9eee53095f51830cc
miczoc98 <miczoc98@gmail.com>
2020-06-14 01:08:05 +0200

    updated autoencoder

--- image_recognition/encoder/autoencoder.py
+++ image_recognition/encoder/autoencoder.py
@@ -1,64 +1,45 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import pathlib
-from os import listdir
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 
-images = listdir('images/')
-image_array = []
-for image in images:
-    image = Image.open('images/' + image)
-    image = image.resize((128, 128))
-    image = np.array(image)/255
-    image_array.append(image)
-image_array = np.asarray(image_array)
-images_train = image_array[0: int(len(images)*.7)]
-images_test = image_array[int(len(images)*.7): -1]
+from tensorflow.keras.models import load_model
 
-images_train = images_train.reshape(
-    len(images_train), np.prod(images_train.shape[1:]))
-images_test = images_test.reshape(
-    len(images_test), np.prod(images_test.shape[1:]))
+import image_load
 
-input_dims = (128, 128, 3)
-input_img = Input(shape=(np.prod(input_dims),))
+test_images, test_labels = image_load.get_images_test()
 
-encoding_dim = 1024
+encoder = load_model('saved/encoder')
+decoder = load_model('saved/decoder')
 
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-decoded = Dense(128*128*3, activation='sigmoid')(encoded)
+encoder.compile()
 
-autoencoder = Model(input_img, decoded)
-encoder = Model(input_img, encoded)
-
-encoded_input = Input(shape=(encoding_dim,))
-decoder_layer = autoencoder.layers[-1]
-decoder = Model(encoded_input, decoder_layer(encoded_input))
-
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-autoencoder.fit(images_train, images_train, epochs=15, batch_size=32,
-                shuffle=True, validation_data=(images_test, images_test))
-
-
-encoded_imgs = encoder.predict(images_test)
+encoded_imgs = encoder.predict(test_images)
 decoded_imgs = decoder.predict(encoded_imgs)
 
+X = []
+Y = []
 
-n = 10
-plt.figure(figsize=(16, 8))
-for i in range(n):
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(images_test[i].reshape(128, 128, 3))
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+for img in encoded_imgs:
+    X.append(img[0])
+    Y.append(img[1])
 
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(128, 128, 3))
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+for i in range(10):
+    plt.scatter(X[i * 10: i * 10 + 10],
+                Y[i * 10: i * 10 + 10], label=test_labels[i * 10])
+
+plt.legend()
 plt.show()
+
+###Plot images###
+# n = 10
+# plt.figure(figsize=(16, 8))
+# for i in range(n):
+#     ax = plt.subplot(2, n, i + 1)
+#     plt.imshow(test_images[i + 100].reshape(128, 128, 3))
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+
+#     ax = plt.subplot(2, n, i + 1 + n)
+#     plt.imshow(decoded_imgs[i + 100].reshape(128, 128, 3))
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+# plt.show()
