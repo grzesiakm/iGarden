207b6d5e1ffe8f4c6678b56c6e0331765ab4d68b
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-06-22 09:32:00 +0200

    building

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 add . /code/
+EXPOSE 3000
 cmd python manage.py migrate && python manage.py runserver 0.0.0.0:8000
--- django_igarden/settings.py
+++ django_igarden/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import django_heroku
+# import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,4 +129,4 @@ LOGIN_REDIRECT_URL = 'igarden-home'
 LOGIN_URL = 'login'
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
